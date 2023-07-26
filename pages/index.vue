c84b0747b1be61a09aafa12cbe0cc70158019128
Sandro Soria <sandro33810@gmail.com>
2023-07-26 13:58:18 +0200

    Thanks DEPENDABOT

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="portfolio">
+    <h1>{{ name }}</h1>
+    <p>Âge : {{ age }}</p>
+    <p>Métier souhaité : {{ profession }}</p>
+    <p>Description :</p>
+    <p>{{ description }}</p>
+    <!-- Ajoutez d'autres informations à afficher ici -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "Votre Nom",
+      age: 25,
+      profession: "Développeur Web",
+      description: "Je suis passionné par le développement web et j'aimerais partager mes projets avec le monde !",
+      // Ajoutez d'autres données personnelles ici
+    };
+  },
+};
+</script>
+
+<style>
+.portfolio {
+  /* Ajoutez des styles CSS personnalisés ici */
+}
+</style>
